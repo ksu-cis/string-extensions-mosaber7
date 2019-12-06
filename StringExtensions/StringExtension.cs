98b8aabee8d5fd91eab730a7aed8048b24b3e7ea
Mohamed Fares <mosaber7@users.campus>
2019-12-06 16:13:18 -0600

    class

---
+++ StringExtensions/StringExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringExtensions
+{
+    public static class StringExtension
+    {
+        public static string capatalize(this string str)
+        {
+
+            string first = str[0].ToString().ToUpper();
+            return first[0] + str.Substring(1);
+
+        }
+        public static string ToCamelCase(this string str)
+        {
+
+
+
+        }
+        public static int WordCount(this string str)
+        {
+
+            return str.Split(new char[] { ' ', '.', '\n', '\t' },StringSplitOptions.RemoveEmptyEntries).Length;
+
+        }
+
+        public static string Smurf(this string s, string toreplce)
+        {
+            return s.Replace(toreplce, "Smurf");
+        }
+    }
+}
